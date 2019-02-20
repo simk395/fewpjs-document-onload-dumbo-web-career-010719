a83fb867b0d1c325ac6767d4c1b08014dc643f0b
simk395 <simon.kong95@gmail.com>
2019-02-19 21:32:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+function domContentLoaded(){
+document.addEventListener("DOMContentLoaded", function(event){
+    updateDOM();
+    });
+}
+
+function updateDOM(){
+    const id = document.querySelector('#text');
+    id.innerHTML = "This is really cool!"
+    return id;
+}
